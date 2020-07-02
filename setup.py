35bdc84bda10785f6bd0e7223698c7495cf69355
Tony Hirst <tony.hirst@gmail.com>
2020-07-02 11:54:39 +0100

    I just donlt understand this setup at all?

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+
+setup(
     name="nb-tensorflow-playground-serverproxy",
-    version='1.0.1',
-    url="https://github.com/innovationOUtside/nb_tensorflow_playground_serverproxy",
-    author="Tony Hirst",
-    description="tony.hirst@gmail.com",
     packages= ['nb_tensorflow_playground_serverproxy'],
+    version='0.0.1',
     include_package_data=True,
-	keywords=['Jupyter'],
-	classifiers=['Framework :: Jupyter'],
     install_requires=[
         'jupyter-server-proxy',
         'notebook'
     ],
+    url="https://github.com/innovationOUtside/nb_tensorflow_playground_serverproxy",
+    author="Tony Hirst",
+    description="tony.hirst@gmail.com",
     entry_points={
         'jupyter_serverproxy_servers': [
             'nb_tensorflow_playground_serverproxy = nb_tensorflow_playground_serverproxy:setup_nb_tensorflow_playground_serverproxy',
         ]
-    },
-    data_files=[],
-    zip_safe=False
+    }
 )
