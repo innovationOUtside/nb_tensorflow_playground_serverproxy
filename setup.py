530ec11e8b3ba6ddef2ab567120e091b6d087abd
Tony Hirst <tony.hirst@gmail.com>
2020-07-02 11:02:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,18 @@ setuptools.setup(
     url="https://github.com/innovationOUtside/nb_tensorflow_playground_serverproxy",
     author="Tony Hirst",
     description="tony.hirst@gmail.com",
-    packages=setuptools.find_packages(),
+    packages= ['nb_tensorflow_playground_serverproxy'],
 	keywords=['Jupyter'],
 	classifiers=['Framework :: Jupyter'],
     install_requires=[
-        'jupyter-server-proxy'
+        'jupyter-server-proxy',
+        'notebook'
     ],
     entry_points={
         'jupyter_serverproxy_servers': [
             'nb_tensorflow_playground_serverproxy = nb_tensorflow_playground_serverproxy:setup_nb_tensorflow_playground_serverproxy',
         ]
     },
-    include_package_data=True,
+    data_files=[],
+    zip_safe=False
 )
